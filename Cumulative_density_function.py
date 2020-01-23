1673a7326243ddbff1cb46ab45ddc6ba84da4a2d
Umar <38042220+pytmar@users.noreply.github.com>
2020-01-23 16:03:01 +0300

    Update Cumulative_density_function.py

--- Cumulative_density_function.py
+++ Cumulative_density_function.py
@@ -10,10 +10,11 @@ def cum_density(arr):
     for num in arr:
         
         emp_list.append(1- ((min_value/num)**mode))
-    return emp_list
+        dictionary = dict(zip(emp_list, arr.values()))
+        
+    return dictionary
 
 arr=[3, 4, 6, 2, 5, 3, 6, 4, 6, 6, 9, 8, 7]
 cum_density(arr)
-
 
 

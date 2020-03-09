7cd21ebac22380311ccba5a4a092788cfa26fe43
Umar <38042220+pytmar@users.noreply.github.com>
2020-03-10 00:01:14 +0300

    Update Wave-Taylor-Functions.py

--- Wave-Taylor-Functions.py
+++ Wave-Taylor-Functions.py
@@ -1,5 +1,6 @@
-def derivative(x, y):
+def der(x):#derivative function
     n=len(x)
+    y=list(range(n))
     sx, sy, sxx, sxy, s, st=0, 0, 0, 0, 0, 0
     for i in range(n):
         sx +=x[i]
@@ -9,18 +10,15 @@ def derivative(x, y):
         
     slope = (n*sxy - sx*sy)/(n*sxx - sx*sx)# comes from y=a*x + b fit line
     return slope
-    
-def wave_function(position, time):
-    if  np.dot(derivative(position, time),derivative(position, time))==1:
-        print('normalized')
-    else:
-        print('not-normalized')
 def fact(n):
     return 1 if (n==1 or n==0) else n * fact(n - 1)
-def taylor(x): #needs some fixing
+def f(m):
+    return m*der(x) + 0.1#slope function
+def taylor(x, a): #needs some fixing
     x=np.array(x)
+    element=x[0]
     n=len(x)
-    tay=[]
-    for i in x:
-        tay.append((x[i]**n)/fact(n))
+    der=derivative(f(x), a, dx=1e-6)
+    for j in x:    
+        tay=(der/fact(x))*((x[j]-a)**n)
     return tay

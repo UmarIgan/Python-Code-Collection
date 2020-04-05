13b481cfc53a48c5d77d69fdcba542bbea550509
Umar <38042220+pytmar@users.noreply.github.com>
2020-04-05 21:52:48 +0300

    Update Probability-mass-function.py

--- Probability-mass-function.py
+++ Probability-mass-function.py
@@ -1,8 +1,7 @@
-import numpy as np
-   
-def factorial(n):
-    return 1 if (n==1 or n==0) else n * factorial(n - 1)
+from statistics import mean
+from math import exp, factorial
+
 def prob_mass(X, event_k):
     for i in event_k:
-        return ((np.mean(X)**event_k[i])*np.exp(-np.mean(X)))/factorial(event_k[i])
+        return ((mean(X)**event_k[i])*exp(-mean(X)))/factorial(event_k[i])
  
